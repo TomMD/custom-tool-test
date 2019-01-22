14b6c6803661b4f6e6e920b75e183684559a0dc4
Thomas M. DuBuisson <tommd@musedev.io>
2019-01-22 15:43:56 -0800

    bar

--- test.sh
+++ test.sh
@@ -3,25 +3,25 @@
 if [ "$1" = "applicable" ] ; then
     echo "true"
 else
-    printf '                          \
-    {                                 \
-      "tag": "ToolSuccess",           \
-      "contents": [                   \
-        [                             \
-          {                           \
-            "tnType": "Type",         \
-            "tnDesc": "Description",  \
-            "tnFile": "File",         \
-            "tnLine": 42,             \
-            "tnColumn": null,         \
-            "tnPatch": null,          \
-            "tnCodeLine": null,       \
-            "tnProcedure": null,      \
-            "tnPhase": "PhaseUnknown",\
-            "tnTool": { "tag" :  "CustomTool", "contents" : "test.sh" }  \
-          }                           \
-        ],                            \
-        null                          \
-      ]                               \
+    printf '                          
+    {                                 
+      "tag": "ToolSuccess",           
+      "contents": [                   
+        [                             
+          {                           
+            "tnType": "Type",         
+            "tnDesc": "Description",  
+            "tnFile": "File",         
+            "tnLine": 42,             
+            "tnColumn": null,         
+            "tnPatch": null,          
+            "tnCodeLine": null,       
+            "tnProcedure": null,      
+            "tnPhase": "PhaseUnknown",
+            "tnTool": { "tag" :  "CustomTool", "contents" : "test.sh" }
+          }                           
+        ],                            
+        null                          
+      ]                               
     }'
 fi
